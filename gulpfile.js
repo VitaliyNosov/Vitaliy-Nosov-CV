f85ae5b696d99cce26059135e4fef12071d25203
VitaliyNosov <webdevelorestudio2017@gmail.com>
2021-12-09 21:25:48 +0200

    add preloader

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('browser-sync', function() {
 		notify: false,
 		// open: false,
 		// online: false, // Work Offline Without Internet Connection
-		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+		tunnel: true, tunnel: "projectname", // Demonstration page: `http://projectname.localtunnel.me`
 	})
 });
 
@@ -45,6 +45,7 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/GSAP/gsap.min.js',
 		'app/libs/fontawesome/fontawesome.js',
 		'app/libs/accardion/accardion.js',
 		'app/libs/custom-file-input/custom-file-input.js',
@@ -54,6 +55,7 @@ gulp.task('scripts', function() {
 		'app/libs/translate/translate.js',
 		'app/libs/typed/typed.min.js',
 		'app/js/common.js', // Always at the end
+		'app/libs/preloader/preloader.js',
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
